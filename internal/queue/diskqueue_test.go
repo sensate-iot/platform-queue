e3e8ed93faf20277d869d00051640e8347863a0a
Michel Megens <michel@michelmegens.net>
2021-05-11 23:07:04 +0200

    queue: disk-queue: add unit tests
    Implement unit tests to test the creation and loading of new and
    existing queues.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- internal/queue/diskqueue_test.go
+++ internal/queue/diskqueue_test.go
@@ -33,6 +33,34 @@ func TestDiskQueue_CannotCreateOnExistingDirectory(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
+
+func TestDiskQueue_New(t *testing.T) {
+	q, err := createDiskQueue()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, q)
+
+	diskQ, ok := q.(*DiskQueue)
+
+	assert.True(t, ok)
+	assert.Equal(t, diskQ.firstSegment.sequence, 1)
+	assert.Equal(t, diskQ.firstSegment.memoryQueue.Capacity(), 128)
+	assert.Equal(t, diskQ.lastSegmentSequenceNumber, 1)
+}
+
+func TestDiskQueue_LoadExisting(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "SegTest")
+	q1, err := NewDiskQueue(dir, "TestQueue", buildDiskQueueInterface, 100)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, q1)
+
+	q2, err := NewDiskQueue(dir, "TestQueue", buildDiskQueueInterface, 100)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, q2)
+}
+
 func TestDiskQueue_Size(t *testing.T) {
 	q,err := createDiskQueue()
 
